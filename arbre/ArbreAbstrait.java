4f8fc4b315c3bada1584fb8ab42f9acef209c77f
Jean-Marc Debicki <debicki3u@fst-ats-311-l19.fst.site.univ-lorraine.fr>
2016-01-21 16:50:35 +0100

    ajout des fichiers de la prof

---
+++ arbre/ArbreAbstrait.java
@@ -0,0 +1,14 @@
+package plic.arbre;
+
+/**
+ * 3 déc. 2015
+ *
+ * @author brigitte wrobel-dautcourt
+ */
+
+public abstract class ArbreAbstrait {
+    
+    protected ArbreAbstrait() {
+    }
+    
+}
---
+++ arbre/expression/ConstanteEntiere.java
@@ -0,0 +1,15 @@
+package plic.arbre.expression;
+
+/**
+ * 3 déc. 2015
+ *
+ * @author brigitte wrobel-dautcourt
+ */
+
+public class ConstanteEntiere extends Constante {
+    
+    public ConstanteEntiere(String texte) {
+        super(texte) ;
+    }
+
+}
